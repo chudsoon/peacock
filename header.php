39e90820848875ca66b0a6b6d1e4ae7e10dc685c
chudsoon <cchudy@gmail.com>
2014-11-21 12:32:39 +0100

    header

--- header.php
+++ header.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php wp_title(); ?></title>
+   <link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"> 
 	
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
