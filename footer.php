70a2f690fac4286b0d86166581e754fdf56f73e5
chudsoon <cchudy@gmail.com>
2014-12-01 15:30:10 +0100

    poprawnie dzialajacy scroll button

--- footer.php
+++ footer.php
@@ -1,11 +1,8 @@
 
-
-
-		<span id="top-link-block" class="">
-			<div class="col-md-12 hidden-xs"  onclick="$('html,body').animate({scrollTop:0},'slow');return false;">
-				<a href="#navbar"><img class="top-button" src="http://1.bp.blogspot.com/-Uy7rJ-k9pmY/Uvo_7PHNNhI/AAAAAAAAANU/Bq8jQoS-M4M/s1600/backTopOff.png"></a>
+		<div class="row">
+			<div class="col-md-12 hidden-xs">
+				<a href="#" class="scrollToTop" ><img src="<?php bloginfo('template_url'); ?>/images/scroll.png"></a>
 			</div>
-		</span>
 		</div>
 		
 		</div>
@@ -32,6 +29,7 @@
  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
  <!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="<?php bloginfo('template_url'); ?>/js/theme.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/holder.js"></script>
 </body>
